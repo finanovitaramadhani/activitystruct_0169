ecdaaa72468183d4f53738fd0d1688fc577510c9
finanovita <finanovita479@gmail.com>
2024-04-02 11:14:43 +0700

    menampilkan data

--- strukturD.2/strukturD.2.cpp
+++ strukturD.2/strukturD.2.cpp
@@ -32,6 +32,14 @@ int main(){
 
     cout << "Umur mahasiswa :";
     cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n Nim :" << mhs.nim;
+    cout << "\n Nama :" << mhs.nama;
+    cout << "\n alamat :";
+    cout << "\n Desa :" << mhs.alamat.desa;
+    cout << "\n Kota :" << mhs.alamat.kota;
+    cout << "\n Umur :" << mhs.umur;
 }
 
 
